e45d5c5f98e32dd083c0382d048b6a89f42b7e22
sebn <sebastien.nicouleaud@gmail.com>
2009-12-10 22:35:26 +0100

    Added :use_slug option

--- lib/matchers/have_friendly_id_matcher.rb
+++ lib/matchers/have_friendly_id_matcher.rb
@@ -3,8 +3,11 @@ module Remarkable
     module Matchers
       class HaveFriendlyIdMatcher < Remarkable::ActiveRecord::Base
         arguments :column
+        optionals :use_slug
+        
         assertions :has_friendly_id?,
-                   :column_matches?
+                   :column_matches?,
+                   :uses_slug?
         
         before_assert do
           if @subject.respond_to?(:friendly_id_options)
@@ -20,6 +23,11 @@ module Remarkable
         
         def column_matches?
           @friendly_id_options[:column] == @column
+        end
+        
+        def uses_slug?
+          return true if @options[:use_slug].nil?
+          @friendly_id_options[:use_slug] == @options[:use_slug]
         end
         
         def interpolation_options
--- spec/have_friendly_id_matcher_spec.rb
+++ spec/have_friendly_id_matcher_spec.rb
@@ -3,37 +3,56 @@ require 'spec_helper'
 describe 'have_friendly_id' do
   include ModelBuilder
   
-  before(:each) do
-    @subject = define_model :users, :login => :string do
+  def define_matcher(column, options={})
+    @model = define_model :users, :login => :string do
       has_friendly_id :login
     end
+
+    have_friendly_id(column, options)
   end
   
   describe 'messages' do
     it 'should contain a description' do
-      @matcher = have_friendly_id(:login)
+      @matcher = define_matcher(:login)
       @matcher.description.should == 'have a friendly id generated from #login'
+      
+      @matcher.use_slug(true)
+      @matcher.description.should == 'have a friendly id generated from #login using a slug'
     end
     
     it 'should set has_friendly_id? message' do
-      @matcher = have_friendly_id(:login)
+      @matcher = define_matcher(:login)
       @matcher.matches? define_model(:items)
       @matcher.failure_message.should == 'Expected Item to have a friendly id, but none was found (did you call the #has_friendly_id macro on your model?)'
     end
     
     it 'should set column_matches? message' do
-      @matcher = have_friendly_id(:name)
-      @matcher.matches? @subject
+      @matcher = define_matcher(:name)
+      @matcher.matches? @model
       @matcher.failure_message.should == 'Expected User to generate its friendly id from #name, not from #login'
+    end
+    
+    it 'should set use_slug_matches? message' do
+      @matcher = define_matcher(:login, :use_slug => true)
+      @matcher.matches? @model
+      @matcher.failure_message.should == 'Expected User to generate its friendly id using a slug'
+    end
+  end
+  
+  before(:each) do
+    @subject = define_model :pages, :title => :string do
+      has_friendly_id :title, :use_slug => true
     end
   end
   
   describe 'matchers' do
-    it { should have_friendly_id :login }
+    it { should have_friendly_id :title }
+    it { should have_friendly_id :title, :use_slug => true }
   end
   
   describe 'macros' do
-    should_have_friendly_id :login
+    should_have_friendly_id :title
+    should_have_friendly_id :title, :use_slug => true
   end
 end
 
